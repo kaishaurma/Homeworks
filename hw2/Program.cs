fe71efb21ecea7b84db050bb0990d6948d25b2e9
unknown <ananokaishauri@gmail.com>
2023-12-13 21:03:34 +0400

    first branch

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Tbiliso!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Anano Kaishauri");
+        Console.WriteLine(Console.ReadLine());
+    }
+}
